698b0bc1f7bc1ccd6d7be200e2536e84854b72e9
emil-nabil333 <elbrinselomda@yahoo.com>
2025-07-05 19:37:57 +0300

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -2,32 +2,13 @@
 # ###########################################
 # SCRIPT : DOWNLOAD AND INSTALL Channel
 # ###########################################
-# Command: wget https://raw.githubusercontent.com/emilnabil/channel-hazem-wahba/main/installer.sh -qO - | /bin/sh
+# Command:
+# wget https://raw.githubusercontent.com/emilnabil/channel-hazem-wahba/main/installer.sh -qO - | /bin/sh
 
 TMPDIR='/tmp'
 PACKAGE='astra-sm'
 MY_URL='https://raw.githubusercontent.com/emilnabil/channel-hazem-wahba/main'
-
 VERSION=$(wget $MY_URL/version -qO- | cut -d "=" -f2-)
-
-BINPATH=/usr/bin
-ETCPATH=/etc
-ASTRAPATH=${ETCPATH}/astra
-
-BBCPMT=${BINPATH}/bbc_pmt_starter.sh
-BBCPY=${BINPATH}/bbc_pmt_v6.py
-BBCENIGMA=${BINPATH}/enigma2_pre_start.sh
-
-SYSCONF=${ETCPATH}/sysctl.conf
-ASTRACONF=${ASTRAPATH}/astra.conf
-ABERTISBIN=${ASTRAPATH}/scripts/abertis
-
-CONFIGpmttmp=${TMPDIR}/bbc_pmt_v6/bbc_pmt_starter.sh
-CONFIGpytmp=${TMPDIR}/bbc_pmt_v6/bbc_pmt_v6.py
-CONFIGentmp=${TMPDIR}/bbc_pmt_v6/enigma2_pre_start.sh
-CONFIGsysctltmp=${TMPDIR}/${PACKAGE}/sysctl.conf
-CONFIGastratmp=${TMPDIR}/${PACKAGE}/astra.conf
-CONFIGabertistmp=${TMPDIR}/${PACKAGE}/abertis
 
 if [ -f /etc/opkg/opkg.conf ]; then
     STATUS='/var/lib/opkg/status'
@@ -36,12 +17,21 @@ if [ -f /etc/opkg/opkg.conf ]; then
     OPKGINSTAL='opkg install'
 fi
 
-rm -rf /etc/enigma2/lamedb /etc/enigma2/*list /etc/enigma2/*.tv /etc/enigma2/*.radio /etc/tuxbox/*.xml
+# detect image name
+if [ -f /etc/image-version ]; then
+    IMAGE_NAME=$(cat /etc/image-version)
+elif [ -f /etc/version ]; then
+    IMAGE_NAME=$(cat /etc/version)
+else
+    IMAGE_NAME=$(uname -s)
+fi
+
+rm -rf /etc/enigma2/lamedb /etc/enigma2/*list /etc/enigma2/*.tv /etc/enigma2/*.radio
 
 install() {
     if ! grep -qs "Package: $1" $STATUS; then
         $OPKG >/dev/null 2>&1
-        echo "   >>>>   Need to install $1   <<<<"
+        echo ">> Installing required package: $1"
         $OPKGINSTAL "$1" >/dev/null 2>&1
         sleep 1
     fi
@@ -53,98 +43,29 @@ if [ "$OSTYPE" = "Opensource" ]; then
     done
 fi
 
-case $(uname -m) in
-    armv7l*) plarform="arm" ;;
-    mips*) plarform="mips" ;;
-esac
-
-rm -rf ${ASTRACONF} ${SYSCONF}
-rm -rf ${TMPDIR}/channels_backup_user_"${VERSION}"* astra-* bbc_pmt_v6*
-
 echo
-set -e
-echo "Downloading And Installing Channel Please Wait ......"
+echo ">> Downloading and installing channel list..."
 wget $MY_URL/channels_hazem-wahba.tar.gz -qP $TMPDIR
 tar -zxf $TMPDIR/channels_hazem-wahba.tar.gz -C /
 sleep 5
-set +e
+
 echo
-echo "   >>>>   Reloading Services - Please Wait   <<<<"
+echo ">> Reloading services..."
 wget -qO - http://127.0.0.1/web/servicelistreload?mode=0 >/dev/null 2>&1
 sleep 2
-echo
-
-if [ -f $BBCPMT ] && [ -f $BBCPY ] && [ -f $BBCENIGMA ]; then
-    echo "   >>>>   All Config BBC Files found   <<<<"
-    sleep 2
-else
-    set -e
-    echo "Downloading And Installing Config BBC Please Wait ......"
-    wget $MY_URL/bbc_pmt_v6.tar.gz -qP $TMPDIR
-    tar -xzf $TMPDIR/bbc_pmt_v6.tar.gz -C $TMPDIR
-    set +e
-    echo "---------------------------------------------"
-    if [ ! -f $BBCPMT ]; then
-        cp -f $CONFIGpmttmp $BBCPMT >/dev/null 2>&1
-        chmod 755 $BBCPMT
-        echo "[send (bbc_pmt_starter.sh) file]"
-    fi
-    if [ ! -f $BBCPY ]; then
-        cp -f $CONFIGpytmp $BBCPY >/dev/null 2>&1
-        chmod 755 $BBCPY
-        echo "[send (bbc_pmt_v6.py) file]"
-    fi
-    if [ ! -f $BBCENIGMA ]; then
-        cp -f $CONFIGentmp $BBCENIGMA >/dev/null 2>&1
-        chmod 755 $BBCENIGMA
-        echo "[send (enigma2_pre_start.sh) file]"
-    fi
-    echo "---------------------------------------------"
-fi
-
-if [ "$OSTYPE" = "Opensource" ]; then
-    if [ -f $ASTRACONF ] && [ -f $ABERTISBIN ] && [ -f $SYSCONF ]; then
-        echo "   >>>>   All Config $PACKAGE Files found   <<<<"
-        sleep 2
-    else
-        set -e
-        echo "Downloading Config $PACKAGE Please Wait ......"
-        wget $MY_URL/astra-"${plarform}".tar.gz -qP $TMPDIR
-        tar -xzf $TMPDIR/astra-"${plarform}".tar.gz -C $TMPDIR
-        set +e
-        echo "---------------------------------------------"
-        if [ ! -f $SYSCONF ]; then
-            cp -f $CONFIGsysctltmp $SYSCONF >/dev/null 2>&1
-            chmod 644 $SYSCONF
-            echo "[send (sysctl.conf) file]"
-        fi
-        if [ ! -f $ASTRACONF ]; then
-            cp -f $CONFIGastratmp $ASTRACONF >/dev/null 2>&1
-            chmod 644 $ASTRACONF
-            echo "[send (astra.conf) file]"
-        fi
-        if [ ! -f $ABERTISBIN ]; then
-            cp -f $CONFIGabertistmp $ABERTISBIN >/dev/null 2>&1
-            chmod 755 $ABERTISBIN
-            echo "[send (abertis) file]"
-        fi
-        echo "---------------------------------------------"
-    fi
-fi
 
 rm -rf ${TMPDIR}/channels_hazem-wahba.tar.gz
-rm -rf ${TMPDIR}/*astra-* ${TMPDIR}/*bbc_pmt_v6*
 sync
 
-echo ""
-echo "*********************************************************"
-echo "#       Channel And Config INSTALLED SUCCESSFULLY       #"
-echo "   UPLOADED BY  >>>>   EMIL_NABIL"
-echo "*********************************************************"
-echo "#                    ${VERSION}                         #"
-echo "*********************************************************"
-echo "#           your Device will RESTART Now                #"
-echo "*********************************************************"
+echo
+echo "**************************************************"
+echo "# Channel and config installed successfully      #"
+echo "# Uploaded by >>>> EMIL_NABIL                    #"
+echo "# Version: ${VERSION}                            #"
+echo "# Image: ${IMAGE_NAME}                           #"
+echo "# System type: ${OSTYPE}                         #"
+echo "# Your device will restart now                   #"
+echo "**************************************************"
 sleep 4
 
 if [ "$OSTYPE" = "Opensource" ]; then
